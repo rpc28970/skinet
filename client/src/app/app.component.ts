89174bb52707b5a866b47f76bf7ee12e1158569e
rpc28970 <rpc28970@gmail.com>
2022-12-14 12:01:57 -0500

    Added NavBar Comp, Started Styling, HttpModule Setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: IProduct[] = []
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response: IPagination) => {this.products = response.data},
+      error: error => console.log(error) 
+    })
+  }
 }
