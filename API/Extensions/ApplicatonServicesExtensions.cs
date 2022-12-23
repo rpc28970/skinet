83217b14b0b6cf47b87031789f6385737b9ae8c6
rpc28970 <rpc28970@gmail.com>
2022-12-23 13:57:42 -0500

    Configured Ordering on the API, Debugging, Unit of Work Pattern

--- API/Extensions/ApplicatonServicesExtensions.cs
+++ API/Extensions/ApplicatonServicesExtensions.cs
@@ -1,5 +1,6 @@
 using API.Errors;
 using Core.Interfaces;
+using Infrastructure;
 using Infrastructure.Data;
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,8 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
